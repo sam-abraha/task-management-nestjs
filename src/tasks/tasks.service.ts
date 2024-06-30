384c6bd0cc3f21befc2cee9408d9f7fb369b592b
sam <samuelabraha@gmail.com>
2024-06-30 22:08:09 +0200

    feat : add getTaskById to TasksController,TaskService

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -11,6 +11,10 @@ export class TasksService {
         return this.tasks;
     }
 
+    getTaskById(id : string) : Task {
+        return this.tasks.find((task) => task.id === task.id);
+    }
+
     createTask(CreateTaskDto : CreateTaskDto) : Task {
 
         const {title , description } = CreateTaskDto;
