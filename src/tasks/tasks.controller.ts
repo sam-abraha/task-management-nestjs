617f2683a4db90855f78ce49c65ec26913b28128
sam <samuelabraha@gmail.com>
2024-07-05 00:24:39 +0200

    feat(tasks): add task creation with user association

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,6 +6,8 @@ import { GetTasksFilterDto } from './dto/filter-task.dto';
 import { UpdateTasksDto } from './dto/update.task.dto';
 import { Task } from './task.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/auth/user.entity';
+import { GetUser } from 'src/auth/get-user.decorator';
 
 @Controller('tasks')
 @UseGuards(AuthGuard())
@@ -18,8 +20,11 @@ export class TasksController {
     }
 
     @Post()
-    createTask(@Body() createTaskDto : CreateTaskDto) : Promise<Task>{
-         return this.tasksService.createTask(createTaskDto);
+    createTask(
+        @Body() createTaskDto : CreateTaskDto,
+        @GetUser() user : User,
+    ) : Promise<Task>{
+         return this.tasksService.createTask(createTaskDto, user);
     }
 
     @Delete('/:id')
