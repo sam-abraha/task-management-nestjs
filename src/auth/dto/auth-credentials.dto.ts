ee2d57fa9a94584e79e2e48876d86c92aab01f46
sam <samuelabraha@gmail.com>
2024-07-04 15:56:32 +0200

    feat : add validtion to password strength and validation in AuthCredentialsDto

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,16 @@
-import { IsNotEmpty } from "class-validator";
+import { IsString, MaxLength, MinLength, Matches} from "class-validator";
 export class AuthCredentialsDto {
 
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username : string;
 
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(8)
+    @MaxLength(32)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: `Password is too weak. Passwords must contain at least: -1 upper case letter, - 1 lower case letter, - 1 number or special character.`,
+      })
     password : string;
 }
