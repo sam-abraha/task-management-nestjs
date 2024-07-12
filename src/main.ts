bcaf9f2d6c5d0a4a36b62ed6551b9efd109879cd
sam <samuelabraha@gmail.com>
2024-07-12 03:12:16 +0200

    feat: add new CORS origin for production URL

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { FilterUserInterceptor } from './filter-user.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ credentials : true, origin : 'http://localhost:5173'});
+  app.enableCors({ 
+    credentials : true, 
+    origin : ['http://localhost:5173', 'https://my-fullstack-task-manager.netlify.app/'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new FilterUserInterceptor())
   await app.listen(3000);
